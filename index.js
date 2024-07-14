96f048cbb63d3abe42d042595538ad3975806d74
Sahaswari Senanayaka <smsahaswari@gmail.com>
2024-07-14 15:38:00 +0530

    Create server for port 3000

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send("Hello world")
+});
+
+//route for the web
+app.listen(port,()=>{
+    console.log(`Server Listening on Part http://localhost:${port}`);
+});
