154693882a3f958f6620edccb7debd2b1ad2af43
Sahaswari Senanayaka <smsahaswari@gmail.com>
2024-07-18 03:31:43 +0530

    Api conecting with database

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -4,8 +4,9 @@ exports.register = async(req, res,next)=>{
     try{
         const{email,password} = req.body;
         const successRes = await UserService.registerUser(email, password);
-        res.josn({status:true,success:"User Registered Succesfully!"});
-        
+       // res.status(200).json({message:"User Registered Succesfully!"});
+        res.json({status:true,success:"User create successfuly."});
+
     }catch(err){
         throw err;
     }
